ba3487fc3ddb4b3d9f436743ccc40cbf4e76eb3d
IlleniumDillon <1476900130@qq.com>
2024-04-03 17:23:14 +0800

    add sim packages

---
+++ src/caterpillar_robot/include/caterpillar_robot/gazebo_ros_modelstate.hpp
@@ -0,0 +1,32 @@
+#ifndef GAZEBO_PLUGINS__GAZEBO_ROS_MODELSTATE_HPP_
+#define GAZEBO_PLUGINS__GAZEBO_ROS_MODELSTATE_HPP_
+
+#include <gazebo/common/Plugin.hh>
+
+#include <memory>
+
+namespace gazebo_plugins
+{
+class GazeboRosModelStatePrivate;
+
+class GazeboRosModelState : public gazebo::ModelPlugin
+{
+public:
+  /// Constructor
+  GazeboRosModelState();
+
+  /// Destructor
+  ~GazeboRosModelState();
+
+protected:
+  // Documentation inherited
+  void Load(gazebo::physics::ModelPtr model, sdf::ElementPtr sdf) override;
+
+private:
+  /// Private data pointer
+  std::unique_ptr<GazeboRosModelStatePrivate> impl_;
+};
+
+}
+
+#endif
---
+++ src/caterpillar_robot/src/gazebo_ros_modelstate.cpp
@@ -0,0 +1,40 @@
+#include <gazebo/common/Events.hh>
+#include <gazebo/common/Time.hh>
+#include <gazebo/physics/Link.hh>
+#include <gazebo/physics/Model.hh>
+#include <gazebo/physics/World.hh>
+#include <gazebo_ros_modelstate.hpp>
+#include <gazebo_ros/conversions/builtin_interfaces.hpp>
+#include <gazebo_ros/node.hpp>
+#ifdef IGN_PROFILER_ENABLE
+#include <ignition/common/Profiler.hh>
+#endif
+#include <rclcpp/rclcpp.hpp>
+#include <std_msgs/msg/float64_multi_array.hpp>
+#include <std_msgs/msg/float64.hpp>
+
+#include <memory>
+#include <string>
+#include <vector>
+
+namespace gazebo_plugins
+{
+class GazeboRosModelStatePrivate
+{
+
+};
+
+GazeboRosModelState::GazeboRosModelState():impl_(std::make_unique<GazeboRosModelStatePrivate>())
+{
+}
+
+GazeboRosModelState::~GazeboRosModelState()
+{
+}
+void GazeboRosModelState::Load(gazebo::physics::ModelPtr model, sdf::ElementPtr sdf)
+{
+}
+
+GZ_REGISTER_MODEL_PLUGIN(GazeboRosModelState)
+
+}
